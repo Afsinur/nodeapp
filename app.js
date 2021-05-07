3d9dfe3a768d1212df2f6e582a758422408fd1d3
mdafsinur <afsin.sayem1@gmail.com>
2021-05-07 11:10:36 +0600

    Alhamdulillah h4 | chatapp v0.1

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
 var express = require("express");
+var portNum = process.env.PORT || "1000";
+var userID;
+var socket = require("socket.io");
 
 var app = express();
 app.set("view engine", "ejs");
 
-var porT = process.env.PORT || "1000";
-app.listen(porT, () => {
-  console.log(`listening on port ${porT}..`);
+//Server setup
+
+//Port setup
+var server = app.listen(portNum, () => {
+  console.log("listening stated..");
 });
 
+//setting static folder
+app.use(express.static("./public"));
+
+//socket setup
+var io = socket(server);
+
+io.on("connect", (socket) => {
+  console.log("connected ID: " + socket.id);
+  userID = socket.id;
+
+  socket.on("chat", (data) => {
+    io.sockets.emit("chat", data);
+  });
+
+  socket.on("typing", (data) => {
+    socket.broadcast.emit("typing", data);
+  });
+});
+
+//get dynamic server
 app.get("/", (req, res) => {
-  var username = "Test_from_pc";
-  res.render("index", { username });
+  var serverRunning = req.headers.host;
+  res.render("index", { serverRunning, userID });
 });
---
+++ public/js/socket.js
@@ -0,0 +1,86 @@
+var matchDataName = [];
+var _direction = "down";
+
+//DOM
+var username = document.getElementById("name"),
+  message = document.getElementById("message"),
+  send = document.getElementById("send"),
+  messages = document.getElementById("messages"),
+  typing = document.getElementById("typing"),
+  chatContainer = document.getElementById("chatContainer");
+
+var _top = chatContainer.scrollTop;
+
+//scroll function
+chatContainer.addEventListener("scroll", () => {
+  var _cur_top = chatContainer.scrollTop;
+
+  if (_top < _cur_top) {
+    _direction = "down";
+  } else {
+    _direction = "up";
+  }
+  _top = _cur_top;
+});
+
+//handlers
+send.addEventListener("click", () => {
+  var io_id = userID;
+  var nameValue = username.value || userID,
+    messageValue = message.value;
+
+  socket.emit("chat", {
+    nameValue,
+    messageValue,
+    io_id,
+  });
+
+  message.value = "";
+});
+
+message.addEventListener("keyup", () => {
+  var nameValueBroadcast = username.value;
+
+  if (message.value != "") {
+    socket.emit("typing", nameValueBroadcast);
+  } else {
+    socket.emit("typing", null);
+  }
+});
+
+//listen for socket
+socket.on("chat", ({ nameValue, messageValue, io_id }) => {
+  if (io_id !== userID) {
+    messages.innerHTML += `<p style="align-self:flex-start;background:#707070;"><span>${nameValue}:<br>${messageValue}</span></p>`;
+  } else {
+    messages.innerHTML += `<p style="align-self:flex-end;background:#444;color:#fff;text-align: right;"><span>you:<br>${messageValue}</span></p>`;
+  }
+
+  typing.innerHTML = "";
+  matchDataName = [];
+
+  if (_direction == "down") {
+    typing.scrollIntoView({
+      behavior: "smooth",
+      block: "end",
+      inline: "nearest",
+    });
+  }
+});
+
+socket.on("typing", (data) => {
+  if (data != null) {
+    if (matchDataName.length == 0) {
+      typing.innerHTML = `<p><em>${data}</em> is tpying a message..</p>`;
+      matchDataName.push(data);
+    } else {
+      if (!matchDataName.includes(data)) {
+        typing.innerHTML += `<p><em>${data}</em> is tpying a message..</p>`;
+        matchDataName.push(data);
+      }
+    }
+  } else {
+    typing.innerHTML = "";
+    matchDataName = [];
+  }
+});
