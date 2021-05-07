1b90822bd070395164b32f309af4cb488c0a20e9
mdafsinur <afsin.sayem1@gmail.com>
2021-05-07 10:38:28 +0600

    Alhamdulillah hr3

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ var express = require("express");
 var app = express();
 app.set("view engine", "ejs");
 
-app.listen(process.env.PORT, () => {
-  console.log("listening..");
+var porT = process.env.PORT || "1000";
+app.listen(porT, () => {
+  console.log(`listening on port ${porT}..`);
 });
 
 app.get("/", (req, res) => {
-  var username = "afsinur_from_pc";
+  var username = "Test_from_pc";
   res.render("index", { username });
 });
